b631fc3605ce3f35f62523c02a8f4f72fc0ca9d3
Mikhail Polivakha <mikhailpolivakha@email.com>
2025-09-16 12:39:35 +0300

    Polishing: minor polishing

--- master/src/main/java/com/nucleonforge/axile/master/service/discovery/InstancesDiscoverer.java
+++ master/src/main/java/com/nucleonforge/axile/master/service/discovery/InstancesDiscoverer.java
@@ -7,15 +7,22 @@ import org.jspecify.annotations.NonNull;
 import com.nucleonforge.axile.common.domain.Instance;
 
 /**
+ * The SPI interface for discovering {@link Instance instances} of running applications.
  *
+ * <p>
+ * There are, essentially, two ways to configure the deployment of the master and starters:
+ * either master itself needs to discover instances, or the instances register themselves in
+ * the master. This SPI interface exists specifically to implement the first approach.
+ * <p>
+ * Implementations may rely on certain environment to be present, such as K8S or consul, or
+ * Netflix Eureka to for instance.
  *
  * @author Mikhail Polivakha
  */
 public interface InstancesDiscoverer {
 
     /**
-     * Discoverer
-     * @return
+     * Perform actual discovery.
      */
     @NonNull
     Set<@NonNull Instance> discover();
