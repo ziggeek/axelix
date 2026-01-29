76abc0c6945dd78fdfc98a8b098d27ea0e569e24
nikita-kirillov <kirilloffnikita1@gmail.com>
2026-01-29 19:37:30 +0500

    GH-654 Create axelix-spring-boot-3 submodule

--- common/auth/build.gradle.kts
+++ common/auth/build.gradle.kts
@@ -1,9 +1,12 @@
+import Dependencies.springBootVersion
+
 plugins {
     id("common")
 }
 
 dependencies {
     // Test
+    testImplementation(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- common/domain/build.gradle.kts
+++ common/domain/build.gradle.kts
@@ -1,9 +1,12 @@
+import Dependencies.springBootVersion
+
 plugins {
     id("common")
 }
 
 dependencies {
     // Test
+    testImplementation(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.assertj:assertj-core")
